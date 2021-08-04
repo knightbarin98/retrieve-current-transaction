fb07975f58872874fd54dc6bcafb390ea0a412f0
shareDreams <17240009@leon.tecnm.mx>
2021-08-03 22:59:01 -0500

    correccion de conexion y campos entity para respuesta de repositorio

--- src/main/java/com/mrbarin/microservicios/retrievecurrenttransaction/api/entity/Transaction.java
+++ src/main/java/com/mrbarin/microservicios/retrievecurrenttransaction/api/entity/Transaction.java
@@ -2,27 +2,95 @@
 package com.mrbarin.microservicios.retrievecurrenttransaction.api.entity;
 
 
+import javax.annotation.Generated;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@Builder
+
 @Document(collection = "Transactions")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+	"_id",
+    "customerId",
+    "date",
+    "transactionDescription"
+})
+@Generated("jsonschema2pojo")
 public class Transaction {
 	
 	@Id
-	public String _id;
-    public Integer customerId;
-    public String date;
-    public TransactionDescription transactionDescription;
+	@JsonProperty("_id")
+	private String _id;
+    @JsonProperty("customerId")
+    private Integer customerId;
+    @JsonProperty("date")
+    private String date;
+    @JsonProperty("transactionDescription")
+    private TransactionDescription transactionDescription;
 
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Transaction() {
+    }
+
+    /**
+     * @param _id
+     * @param date
+     * @param customerId
+     * @param transactionDescription
+     */
+    public Transaction(String _id, Integer customerId, String date, TransactionDescription transactionDescription) {
+        super();
+        this._id = _id;
+        this.customerId = customerId;
+        this.date = date;
+        this.transactionDescription = transactionDescription;
+    }
+    
+    @JsonProperty("_id")
+    public String getId() {
+        return _id;
+    }
+
+    @JsonProperty("_id")
+    public void setCustomerId(String _id) {
+        this._id = _id;
+    }
+
+    @JsonProperty("customerId")
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    @JsonProperty("customerId")
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    @JsonProperty("date")
+    public String getDate() {
+        return date;
+    }
+
+    @JsonProperty("date")
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @JsonProperty("transactionDescription")
+    public TransactionDescription getTransactionDescription() {
+        return transactionDescription;
+    }
+
+    @JsonProperty("transactionDescription")
+    public void setTransactionDescription(TransactionDescription transactionDescription) {
+        this.transactionDescription = transactionDescription;
+    }
 }
--- src/main/java/com/mrbarin/microservicios/retrievecurrenttransaction/api/entity/TransactionDescription.java
+++ src/main/java/com/mrbarin/microservicios/retrievecurrenttransaction/api/entity/TransactionDescription.java
@@ -3,21 +3,93 @@ package com.mrbarin.microservicios.retrievecurrenttransaction.api.entity;
 
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import javax.annotation.Generated;
 
-@Data
-@Getter
-@Setter
-@AllArgsConstructor
-@Builder
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "TransactionType",
+    "Descripcion",
+    "amount"
+})
+@Generated("jsonschema2pojo")
 public class TransactionDescription {
+	
+	
+	@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("TransactionType")
+	@Field("TransactionType")
+    private String transactionType;
+	
+	@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("Descripcion")
+	@Field("Descripcion")
+    private String descripcion;
+    @JsonProperty("amount")
+    private Float amount;
 
-    public String transactionType;
-    public String descripcion;
-    public Float amount;
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public TransactionDescription() {
+    }
+
+    /**
+     * 
+     * @param transactionType
+     * @param descripcion
+     * @param amount
+     */
+    public TransactionDescription(String transactionType, String descripcion, Float amount) {
+        super();
+        this.transactionType = transactionType;
+        this.descripcion = descripcion;
+        this.amount = amount;
+    }
+    
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("TransactionType")
+    @Field("TransactionType")
+    public String getTransactionType() {
+        return transactionType;
+    }
+    
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("TransactionType")
+    @Field("TransactionType")
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+    
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("Descripcion")
+    @Field("Descripcion")
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+    @JsonProperty("Descripcion")
+    @Field("Descripcion")
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @JsonProperty("amount")
+    public Float getAmount() {
+        return amount;
+    }
+
+    @JsonProperty("amount")
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
 
 }
